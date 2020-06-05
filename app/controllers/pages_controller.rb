153ef2bf32b3039ecf6ebe2164adbad7c0efe564
BenjaminDorsinvil <bdorsinvil@gmail.com>
2020-06-05 00:12:55 -0400

    updated the links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 
 	def home 
+		@files = Dir.glob('*')
 	end 
 
 	def about 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :articles
   root 'pages#home'
 
   get 'about', to: 'pages#about'
+  get 'home', to: 'pages#home'
+  resources :articles
 end
