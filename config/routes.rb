d66284dab73f4fe32bc3c4b3f81201a2500ddc66
BenjaminDorsinvil <bdorsinvil@gmail.com>
2020-06-07 00:58:58 -0400

    clean up layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :articles
-  root 'pages#home'
 
-  get 'about', to: 'pages#about'
-  get 'home', to: 'pages#home'
+  root 'pages#home'
   resources :articles
+  
+  get 'home', to: 'pages#home'
+  get 'about', to: 'pages#about'
 end
